89572bd8a7f7ab776587099d6a88b747b493faea
SEMO\tlstp <tlstpal36@gmail.com>
2024-03-26 07:13:40 +0900

    Feat : 색상 및 폰트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        black: "#191A20",
+        deepBlue: "#282A35",
+        blue: "#43486B",
+        white: "#F8F8F2",
+        orange: "#FFB86C",
+        skyblue: "#8BE9FD",
+        green: "#50FA7B",
+        pink: "#FF79C6",
+        red: "#FF4E59",
+        yellow: "#F0DB6D",
+        deepYellow: "#444444"
+      },
+      tonFamily:{
+        koreanFont:"Pretendard-Regular",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
     },
   },
   plugins: [],
