8bbb02f296e4368f910804d098f1e8e0ae5b0aeb
haegaea <raeweric@gmail.com>
2024-04-10 21:59:13 +0900

    Fix : 전공목록 출력 기능 버그 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import InputBox from './InputBox';
 import Dropdown from './Dropdown';
 import { useRouter } from 'next/navigation';
+import Header from '../ComponentsHeader';
 import axios from 'axios'; // Axios를 사용하여 서버 API 호출
 
 
@@ -51,12 +52,12 @@ const SignupPage: React.FC = () => {
       const response = await axios.get('https://f8595853-a026-42f2-a444-0fcbd28602b0.mock.pstmn.io/signup');
       console.log('응답 데이터:', response.data);
       if (Array.isArray(response.data)) {
-        const majors = response.data.map((item: any) => item.major);
-        console.log('전공 목록:', majors);
-        setMajors(majors); // 전공 목록 설정
+        const names = response.data.map((item: any) => item.name); // "name" 값을 추출하여 새로운 배열 생성
+        console.log('전공 목록:', names);
+        setMajors(names); // 추출된 "name" 값을 전공 목록으로 설정
         setFormData(prevFormData => ({
           ...prevFormData,
-          major: majors[0] || '' // 첫 번째 전공을 선택
+          major: names[0] || '' // 첫 번째 전공을 선택
         }));
         setMajorsFetched(true); // 전공 목록을 받아왔음을 표시
       } else {
@@ -66,6 +67,7 @@ const SignupPage: React.FC = () => {
       console.error('Failed to fetch dropdown options:', error);
     }
   };
+  
   
   useEffect(() => {
     if (!majorsFetched) { // 전공 목록을 받아오지 않았다면 요청
@@ -181,152 +183,157 @@ const SignupPage: React.FC = () => {
   };
  
   return (
-    <div className="min-h-screen flex bg-black justify-between px-20">
-      <div className="text-white font-bold text-xl mb-20 ml-40">
-        <h2 className="text-bold text-[26px] mb-6 mt-60">회원가입</h2>
-        <form onSubmit={handleSubmit}>
-          <InputBox
-            label="이름"
-            value={formData.name}
-            error={errors.name.isError}
-            errorMessage={errors.name.message}
-            onChange={(e) => handleChange('name', e.target.value)}
-            placeholder="이름을 입력해주세요."
-            required
-          />
-          <InputBox
-            label="연락처"
-            value={formData.pnumber}
-            error={errors.pnumber.isError}
-            errorMessage={errors.pnumber.message}
-            onChange={(e) => handleChange('pnumber', e.target.value)}
-            placeholder="010-XXXX-YYYY 형식으로 입력해주세요."
-            required
-          />
-          <InputBox
-            label="e-mail"
-            value={formData.email}
-            error={errors.email.isError}
-            errorMessage={errors.email.message}
-            onChange={(e) => handleChange('email', e.target.value)}
-            placeholder="e-mail을 입력해주세요."
-            required
-          />
-          <InputBox
-            label="생년월일"
-            value={formData.birthday}
-            error={errors.birthday.isError}
-            errorMessage={errors.birthday.message}
-            onChange={(e) => handleChange('birthday', e.target.value)}
-            placeholder="YYYY/MM/DD 형식으로 입력해주세요."
-            required
-          />
+    <div>
+      <div>
+        <Header/>
+      </div>
+      <div className="min-h-screen flex bg-black justify-between px-20">
+        <div className="text-white font-bold text-xl mb-20 ml-40">
+          <h2 className="text-bold text-[26px] mb-6 mt-60">회원가입</h2>
+          <form onSubmit={handleSubmit}>
+            <InputBox
+              label="이름"
+              value={formData.name}
+              error={errors.name.isError}
+              errorMessage={errors.name.message}
+              onChange={(e) => handleChange('name', e.target.value)}
+              placeholder="이름을 입력해주세요."
+              required
+            />
+            <InputBox
+              label="연락처"
+              value={formData.pnumber}
+              error={errors.pnumber.isError}
+              errorMessage={errors.pnumber.message}
+              onChange={(e) => handleChange('pnumber', e.target.value)}
+              placeholder="010-XXXX-YYYY 형식으로 입력해주세요."
+              required
+            />
+            <InputBox
+              label="e-mail"
+              value={formData.email}
+              error={errors.email.isError}
+              errorMessage={errors.email.message}
+              onChange={(e) => handleChange('email', e.target.value)}
+              placeholder="e-mail을 입력해주세요."
+              required
+            />
+            <InputBox
+              label="생년월일"
+              value={formData.birthday}
+              error={errors.birthday.isError}
+              errorMessage={errors.birthday.message}
+              onChange={(e) => handleChange('birthday', e.target.value)}
+              placeholder="YYYY/MM/DD 형식으로 입력해주세요."
+              required
+            />
 
-          <Dropdown
-            label="분야"
-            options={["개발", "CERT"]}
-            onSelect={(value) => handleDropdownChange('field', value)}
-            placeholder="분야를 선택해주세요."
-            required
-            error={errors.field.isError}
-          />
-          <Dropdown
-            label="재학 상태"
-            options={['재학', '휴학', '졸업']}
-            onSelect={(value) => handleDropdownChange('status', value)}
-            placeholder="재학 상태를 선택해주세요."
-            required
-            error={errors.status.isError}
-          />
-          <Dropdown
-            label="학년 및 학차"
-            options={['1학년 / 1학차', '1학년 / 2학차', '2학년 / 3학차', '2학년 / 4학차', '3학년 / 5학차', '3학년 / 6학차', '4학년 / 7학차', '4학년 / 8학차', '그 이상', '졸업']}
-            onSelect={(value) => handleDropdownChange('grade', value)}
-            placeholder="학년/학차를 선택해주세요."
-            required
-            error={errors.grade.isError}
-          />
+            <Dropdown
+              label="분야"
+              options={["개발", "CERT"]}
+              onSelect={(value) => handleDropdownChange('field', value)}
+              placeholder="분야를 선택해주세요."
+              required
+              error={errors.field.isError}
+            />
+            <Dropdown
+              label="재학 상태"
+              options={['재학', '휴학', '졸업']}
+              onSelect={(value) => handleDropdownChange('status', value)}
+              placeholder="재학 상태를 선택해주세요."
+              required
+              error={errors.status.isError}
+            />
+            <Dropdown
+              label="학년 및 학차"
+              options={['1학년 / 1학차', '1학년 / 2학차', '2학년 / 3학차', '2학년 / 4학차', '3학년 / 5학차', '3학년 / 6학차', '4학년 / 7학차', '4학년 / 8학차', '그 이상', '졸업']}
+              onSelect={(value) => handleDropdownChange('grade', value)}
+              placeholder="학년/학차를 선택해주세요."
+              required
+              error={errors.grade.isError}
+            />
 
-          <InputBox
-            label="비밀번호"
-            value={formData.password}
-            error={errors.password.isError}
-            errorMessage={errors.password.message}
-            onChange={(e) => handleChange('password', e.target.value)}
-            placeholder="비밀번호를 입력하세요."
-            required
-            type="password"
-          />
-          <InputBox
-            label="비밀번호 확인"
-            value={formData.passwordConfirm}
-            error={errors.passwordConfirm.isError}
-            errorMessage={errors.passwordConfirm.message}
-            onChange={(e) => handleChange('passwordConfirm', e.target.value)}
-            placeholder="비밀번호를 다시 한 번 입력하세요."
-            required
-            type="password"
-          />
-          <InputBox
-            label="학번"
-            value={formData.studentId}
-            error={errors.studentId.isError}
-            errorMessage={errors.studentId.message}
-            onChange={(e) => handleChange('studentId', e.target.value)}
-            placeholder="학번을 입력해주세요."
-            required
-          />
+            <InputBox
+              label="비밀번호"
+              value={formData.password}
+              error={errors.password.isError}
+              errorMessage={errors.password.message}
+              onChange={(e) => handleChange('password', e.target.value)}
+              placeholder="비밀번호를 입력하세요."
+              required
+              type="password"
+            />
+            <InputBox
+              label="비밀번호 확인"
+              value={formData.passwordConfirm}
+              error={errors.passwordConfirm.isError}
+              errorMessage={errors.passwordConfirm.message}
+              onChange={(e) => handleChange('passwordConfirm', e.target.value)}
+              placeholder="비밀번호를 다시 한 번 입력하세요."
+              required
+              type="password"
+            />
+            <InputBox
+              label="학번"
+              value={formData.studentId}
+              error={errors.studentId.isError}
+              errorMessage={errors.studentId.message}
+              onChange={(e) => handleChange('studentId', e.target.value)}
+              placeholder="학번을 입력해주세요."
+              required
+            />
 
-          <Dropdown
-            label="학부/학과(전공)"
-            options={formData.major ? [formData.major] : ["없음"]}
-            onSelect={(value) => handleDropdownChange('major', value)}
-            placeholder="주전공을 선택해주세요."
-            required
-            error={errors.major.isError}
-          />
-          <Dropdown
-            label="부전공"
-            options={formData.major ? [formData.major] : []}
-            onSelect={(value) => handleDropdownChange('minor', value)}
-            placeholder="없으면 비워주세요."
-          />
-          <Dropdown
-            label="복수전공1"
-            options={formData.major ? [formData.major] : []}
-            onSelect={(value) => handleDropdownChange('doubleMajor1', value)}
-            placeholder="없으면 비워주세요."
-          />
-          <Dropdown
-            label="복수전공2"
-            options={formData.major ? [formData.major] : []}
-            onSelect={(value) => handleDropdownChange('doubleMajor2', value)}
-            placeholder="없으면 비워주세요."
-          />
+            <Dropdown
+              label="학부/학과(전공)"
+              options={majors}
+              onSelect={(value) => handleDropdownChange('major', value)}
+              placeholder="주전공을 선택해주세요."
+              required
+              error={errors.major.isError}
+            />
+            <Dropdown
+              label="부전공"
+              options={majors}
+              onSelect={(value) => handleDropdownChange('minor', value)}
+              placeholder="없으면 비워주세요."
+            />
+            <Dropdown
+              label="복수전공1"
+              options={majors}
+              onSelect={(value) => handleDropdownChange('doubleMajor1', value)}
+              placeholder="없으면 비워주세요."
+            />
+            <Dropdown
+              label="복수전공2"
+              options={majors}
+              onSelect={(value) => handleDropdownChange('doubleMajor2', value)}
+              placeholder="없으면 비워주세요."
+            />
 
-          <div className="flex items-center">
-            <div>
-              <div className='mt-12 text-base'>
-                ID는 자신의 학번으로 지정됩니다.
+            <div className="flex items-center">
+              <div>
+                <div className='mt-12 text-base'>
+                  ID는 자신의 학번으로 지정됩니다.
+                </div>
+                <div className='mt-4 text-base'>
+                  전화번호, e-mail 등의 정보는 제출 후 변경할 수 없으니 신중하게 작성해주세요.
+                </div>
               </div>
-              <div className='mt-4 text-base'>
-                전화번호, e-mail 등의 정보는 제출 후 변경할 수 없으니 신중하게 작성해주세요.
+              <div className="flex mt-8 ml-[300px] items-center">
+                {errors.global.isError && (
+                  <div className="absolute text-red text-base mt-2 right-0 mr-[360px]">{errors.global.message}</div>
+                )}
+                
+                <button className="absolute bg-green hover:bg-green text-black font-bold text-base py-2 px-4 rounded w-30 h-10 right-0 mr-60">
+                  제출하기
+                </button>
               </div>
             </div>
-            <div className="flex mt-8 ml-[300px] items-center">
-              {errors.global.isError && (
-                <div className="absolute text-red text-base mt-2 right-0 mr-[360px]">{errors.global.message}</div>
-              )}
-              
-              <button className="absolute bg-green hover:bg-green text-black font-bold text-base py-2 px-4 rounded w-30 h-10 right-0 mr-60">
-                제출하기
-              </button>
-            </div>
-          </div>
 
-        </form>
+          </form>
 
-        
+          
+        </div>
       </div>
     </div>
   );
