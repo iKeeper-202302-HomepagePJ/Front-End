9dbc3edbcf1cc8056d2ac4dc1a5a2bdfd0ca955c
haegaea <raeweric@gmail.com>
2024-04-10 20:15:54 +0900

    Fix : 전공 이름 수정 department -> major Fix : 연락처 이름 수정 contact -> pnumber Fix : 이메일 인증 삭제 Feat : 전공 목록 받아오기 추가(작업중) Fix : 비밀번호 확인 기능 수정 Feat : 회원가입 로직 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,14 +3,17 @@
 import React, { useState, useEffect } from 'react';
 import InputBox from './InputBox';
 import Dropdown from './Dropdown';
+import { useRouter } from 'next/navigation';
 import axios from 'axios'; // Axios를 사용하여 서버 API 호출
 
+
 const SignupPage: React.FC = () => {
+  const router = useRouter();
+
   const [formData, setFormData] = useState({
     name: '',
-    contact: '',
+    pnumber: '',
     email: '',
-    emailVerificationCode: '',
     birthday: '',
     field: '',
     status: '',
@@ -18,7 +21,7 @@ const SignupPage: React.FC = () => {
     password: '',
     passwordConfirm: '',
     studentId: '',
-    department: '',
+    major: '',
     minor: '',
     doubleMajor1: '',
     doubleMajor2: ''
@@ -26,9 +29,8 @@ const SignupPage: React.FC = () => {
 
   const [errors, setErrors] = useState({
     name: { isError: false, message: '' },
-    contact: { isError: false, message: '' },
+    pnumber: { isError: false, message: '' },
     email: { isError: false, message: '' },
-    emailVerificationCode: { isError: false, message: '' },
     birthday: { isError: false, message: '' },
     field: { isError: false, message: '' },
     status: { isError: false, message: '' },
@@ -36,33 +38,44 @@ const SignupPage: React.FC = () => {
     password: { isError: false, message: '' },
     passwordConfirm: { isError: false, message: '' },
     studentId: { isError: false, message: '' },
-    department: { isError: false, message: '' },
+    major: { isError: false, message: '' },
     global: { isError: false, message: '' }
     // 나머지 필수 입력 항목 추가
   });
 
-  useEffect(() => {
-    fetchDropdownOptions(); // 드롭다운 옵션 데이터를 서버에서 가져오는 함수 호출
-  }, []);
+  const [majors, setMajors] = useState<string[]>([]); // 전공 목록 상태 추가
+  const [majorsFetched, setMajorsFetched] = useState(false); // 전공 목록을 이미 받아왔는지 여부 상태 추가
 
   const fetchDropdownOptions = async () => {
     try {
-      const response = await axios.get('/api/options'); // 서버 API 호출하여 드롭다운 옵션 데이터 가져오기
-      const { fieldOptions, statusOptions, gradeOptions, departmentOptions } = response.data; // 예시: 서버에서 필요한 드롭다운 옵션 데이터를 받아와서 분리
-      setFormData({
-        ...formData,
-        field: fieldOptions[0], // 예시: 첫 번째 항목을 기본값으로 설정
-        status: statusOptions[0],
-        grade: gradeOptions[0],
-        department: departmentOptions[0]
-      });
+      const response = await axios.get('https://f8595853-a026-42f2-a444-0fcbd28602b0.mock.pstmn.io/signup');
+      console.log('응답 데이터:', response.data);
+      if (Array.isArray(response.data)) {
+        const majors = response.data.map((item: any) => item.major);
+        console.log('전공 목록:', majors);
+        setMajors(majors); // 전공 목록 설정
+        setFormData(prevFormData => ({
+          ...prevFormData,
+          major: majors[0] || '' // 첫 번째 전공을 선택
+        }));
+        setMajorsFetched(true); // 전공 목록을 받아왔음을 표시
+      } else {
+        console.error('Failed to fetch dropdown options: Response data is not an array');
+      }
     } catch (error) {
       console.error('Failed to fetch dropdown options:', error);
     }
   };
+  
+  useEffect(() => {
+    if (!majorsFetched) { // 전공 목록을 받아오지 않았다면 요청
+      fetchDropdownOptions();
+    }
+  }, [majorsFetched]);
 
   const handleDropdownChange = (key: string, value: string) => {
-    setFormData({ ...formData, [key]: value });
+    const processedValue = value.trim() === '' ? '없음' : value.trim();
+    setFormData({ ...formData, [key]: processedValue });
     setErrors({ ...errors, [key]: { isError: false, message: '' } });
   };
 
@@ -71,63 +84,80 @@ const SignupPage: React.FC = () => {
     setErrors({ ...errors, [key]: { isError: false, message: '' } });
   };
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     let formErrors = { ...errors };
     let isFormValid = true;
-
+  
     // 필수 입력 항목 검사
     if (!formData.name.trim()) {
       formErrors.name = { isError: true, message: '이름을 입력해주세요.' };
+      isFormValid = false; // 필수 입력 항목이 비어 있음을 표시
     }
-    if (!formData.contact.trim()) {
-      formErrors.contact = { isError: true, message: '연락처를 입력해주세요.' };
+    if (!formData.pnumber.trim()) {
+      formErrors.pnumber = { isError: true, message: '연락처를 입력해주세요.' };
+      isFormValid = false;
     }
     if (!formData.email.trim()) {
       formErrors.email = { isError: true, message: '이메일을 입력해주세요.' };
-    }
-    if (!formData.emailVerificationCode.trim()) {
-      formErrors.emailVerificationCode = { isError: true, message: '이메일 인증번호를 입력해주세요.' };
+      isFormValid = false;
     }
     if (!formData.birthday.trim()) {
       formErrors.birthday = { isError: true, message: '생일을 입력해주세요.' };
+      isFormValid = false;
     }
     if (!formData.password.trim()) {
       formErrors.password = { isError: true, message: '비밀번호를 입력해주세요.' };
+      isFormValid = false;
     }
     if (!formData.passwordConfirm.trim()) {
       formErrors.passwordConfirm = { isError: true, message: '비밀번호 확인을 입력해주세요.' };
+      isFormValid = false;
     }
     if (!formData.studentId.trim()) {
       formErrors.studentId = { isError: true, message: '학번을 입력해주세요.' };
+      isFormValid = false;
     }
-
+  
     // Dropdown 컴포넌트의 유효성 검사를 수행합니다.
     const dropdownErrors = validateDropdowns();
     formErrors = { ...formErrors, ...dropdownErrors };
-
+  
     // 필수 입력 항목 또는 드롭다운 항목 중 하나라도 오류가 있는지 확인합니다.
     Object.keys(formErrors).forEach((key) => {
       if (formErrors[key as keyof typeof formErrors].isError) {
         isFormValid = false;
       }
-      
     });
-
-    if (formData.password !== formData.passwordConfirm) {
+  
+    if (formData.password !== formData.passwordConfirm && formData.passwordConfirm !== '') {
       formErrors.passwordConfirm = { isError: true, message: '비밀번호가 같지 않습니다.' };
+      isFormValid = false;
     }
-
+  
     if (!isFormValid) {
       formErrors.global = { isError: true, message: '필수 입력 항목 중 누락된 항목이 있습니다. 다시 확인해주세요.' };
+      setErrors(formErrors); // 오류 메시지 표시 또는 초기화
+      isFormValid = true;
+      return; // 폼 제출 중지
     } else {
-      formErrors.global = { isError: false, message: '' };
-      // 여기에 회원가입 처리 로직을 추가합니다.
+      formErrors.global = { isError: false, message: '' }; // 필수 입력 항목이 모두 입력되었으므로 오류 메시지 초기화
+      setErrors(formErrors); // 오류 메시지 표시 또는 초기화
+      isFormValid = true;
+  }
+  
+    try {
+      // 회원가입 정보를 서버로 전송합니다.
+      const response = await axios.post('https://f8595853-a026-42f2-a444-0fcbd28602b0.mock.pstmn.io/api/signup', formData);
+      console.log('회원가입 성공:', response.data);
+      // 회원가입 성공 시에만 라우팅 수행
+      router.push('/main');
+    } catch (error) {
+      console.error('회원가입 실패:', error);
+      // 여기에 회원가입 실패 처리 로직을 추가합니다.
+      formErrors.global = { isError: true, message: '회원가입에 실패했습니다. 다시 확인해주세요.' };
+      setErrors(formErrors);
     }
-
-    setErrors(formErrors);
-
-    // 여기에 회원가입 처리 로직을 추가합니다.
   };
 
   const validateDropdowns = () => {
@@ -142,8 +172,8 @@ const SignupPage: React.FC = () => {
     if (!formData.grade.trim()) {
       dropdownErrors.grade = { isError: true, message: '학년/학차를 선택해주세요.' };
     }
-    if (!formData.department.trim()) {
-      dropdownErrors.department = { isError: true, message: '학부/학과(전공)을 선택해주세요.' };
+    if (!formData.major.trim()) {
+      dropdownErrors.major = { isError: true, message: '학부/학과(전공)을 선택해주세요.' };
     }
     // 나머지 드롭다운 항목에 대한 검사 추가
 
@@ -166,10 +196,10 @@ const SignupPage: React.FC = () => {
           />
           <InputBox
             label="연락처"
-            value={formData.contact}
-            error={errors.contact.isError}
-            errorMessage={errors.contact.message}
-            onChange={(e) => handleChange('contact', e.target.value)}
+            value={formData.pnumber}
+            error={errors.pnumber.isError}
+            errorMessage={errors.pnumber.message}
+            onChange={(e) => handleChange('pnumber', e.target.value)}
             placeholder="010-XXXX-YYYY 형식으로 입력해주세요."
             required
           />
@@ -180,15 +210,6 @@ const SignupPage: React.FC = () => {
             errorMessage={errors.email.message}
             onChange={(e) => handleChange('email', e.target.value)}
             placeholder="e-mail을 입력해주세요."
-            required
-          />
-          <InputBox
-            label="e-mail 인증번호"
-            value={formData.emailVerificationCode}
-            error={errors.emailVerificationCode.isError}
-            errorMessage={errors.emailVerificationCode.message}
-            onChange={(e) => handleChange('emailVerificationCode', e.target.value)}
-            placeholder="인증번호를 입력해주세요."
             required
           />
           <InputBox
@@ -203,15 +224,15 @@ const SignupPage: React.FC = () => {
 
           <Dropdown
             label="분야"
-            options={formData.field ? [formData.field] : []} // formData에 있는 field를 옵션으로 설정
+            options={["개발", "CERT"]}
             onSelect={(value) => handleDropdownChange('field', value)}
             placeholder="분야를 선택해주세요."
             required
             error={errors.field.isError}
           />
           <Dropdown
-            label="재적 상태"
-            options={formData.status ? [formData.status] : []}
+            label="재학 상태"
+            options={['재학', '휴학', '졸업']}
             onSelect={(value) => handleDropdownChange('status', value)}
             placeholder="재학 상태를 선택해주세요."
             required
@@ -219,13 +240,12 @@ const SignupPage: React.FC = () => {
           />
           <Dropdown
             label="학년 및 학차"
-            options={formData.grade ? [formData.grade] : []}
+            options={['1학년 / 1학차', '1학년 / 2학차', '2학년 / 3학차', '2학년 / 4학차', '3학년 / 5학차', '3학년 / 6학차', '4학년 / 7학차', '4학년 / 8학차', '그 이상', '졸업']}
             onSelect={(value) => handleDropdownChange('grade', value)}
             placeholder="학년/학차를 선택해주세요."
             required
             error={errors.grade.isError}
           />
-
 
           <InputBox
             label="비밀번호"
@@ -259,27 +279,27 @@ const SignupPage: React.FC = () => {
 
           <Dropdown
             label="학부/학과(전공)"
-            options={formData.department ? [formData.department] : []}
-            onSelect={(value) => handleDropdownChange('department', value)}
+            options={formData.major ? [formData.major] : []}
+            onSelect={(value) => handleDropdownChange('major', value)}
             placeholder="주전공을 선택해주세요."
             required
-            error={errors.department.isError}
+            error={errors.major.isError}
           />
           <Dropdown
             label="부전공"
-            options={formData.department ? [formData.department] : []}
+            options={formData.major ? [formData.major] : []}
             onSelect={(value) => handleDropdownChange('minor', value)}
             placeholder="없으면 비워주세요."
           />
           <Dropdown
             label="복수전공1"
-            options={formData.department ? [formData.department] : []}
+            options={formData.major ? [formData.major] : []}
             onSelect={(value) => handleDropdownChange('doubleMajor1', value)}
             placeholder="없으면 비워주세요."
           />
           <Dropdown
             label="복수전공2"
-            options={formData.department ? [formData.department] : []}
+            options={formData.major ? [formData.major] : []}
             onSelect={(value) => handleDropdownChange('doubleMajor2', value)}
             placeholder="없으면 비워주세요."
           />
